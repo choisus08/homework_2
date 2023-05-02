01ecb8744f68a985a76eb1f119466f9314910853
Susie Gordon <susieyoungchoi@gmail.com>
2023-05-01 23:36:29 -0400

    finished loop practice

---
+++ loops.js
@@ -0,0 +1,71 @@
+// LOOP 1
+
+for (i=0; i<100; i++) {
+    console.log(i);
+}
+
+
+// LOOP 2
+
+for (i=99; i>-1; i--) {
+    console.log(i);
+}
+
+
+// LOOP 3
+
+for (i=0; i<99; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
+
+
+// LOOP 4
+
+for (i=98; i>-1; i--) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
+
+
+// LOOP 5
+
+for (i=0; i<100; i++) {
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+}
+
+
+// LOOP 6
+
+for (i=99; i>-1; i--) {
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+}
+
+
+// LOOP 7
+
+for (i=49; i<73; i++) {
+    console.log(i);
+}
+
+
+// LOOP 8
+
+for (i=81; i>25; i--) {
+    console.log(i);
+}
+
+
+// LOOP 9
+
+for (i=3; i<91; i++) {
+    if (i % 3 === 0) {
+        console.log(i);
+    }
+}
